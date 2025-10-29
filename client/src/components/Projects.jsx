77445e0a21fb2546b5daa38b7b58458cdae71858
Prasathb06 <arunprasathb045@gmail.com>
2025-10-29 12:45:29 +0530

    first commit

--- client/src/components/Projects.jsx
+++ client/src/components/Projects.jsx
@@ -9,18 +9,18 @@ const Projects = () => {
             title: "WeLearn Institution",
             description: "Built a full-stack e-commerce and learning platform using React, Node.js, Express, and MongoDB. Developed user authentication, a course marketplace, and an admin dashboard for course management. Designed and integrated REST APIs for secure data handling and scalability. Delivered a fully responsive frontend to improve user accessibility across devices. Enhanced the learning experience by enabling users to register, browse, and purchase courses seamlessly.",
             image: projectimg,
-            deployLink: "https://your-lively-deploy-link.com"
+            deployLink: "https://welearninstitution-1.onrender.com"
         },
         {
-            title: "E-commerce App",
-            description: "A full-featured e-commerce application using Node.js and MongoDB.",
-            image: "https://via.placeholder.com/300x200.png?text=E-commerce",
-            deployLink: "https://your-lively-ecommerce-link.com"
+            title: "Fit_Factory",
+            description: "Fit Factory is a fully responsive front-end fitness e-commerce website designed to promote and sell gym-related products. The platform provides users with an engaging interface to explore workout gear, supplements, and fitness accessories. The website emphasizes modern UI design, smooth navigation, and a professional shopping experience using only client-side technologies.",
+            image: projectimg1,
+            deployLink: "https://fit-factory01.netlify.app/"
         },
         {
             title: "Blog Platform",
             description: "Created a fitness education website for training programs and product sales.Used HTML, CSS, Bootstrap, and JavaScript to design a mobile-friendly, responsive UI.Optimized cross-device compatibility, ensuring 100% accessibility on mobile and desktop.Improved user engagement by developing a visually appealing and easy-to-navigate interface.",
-            image: projectimg1,
+            image: "",
             deployLink: "https://your-lively-blog-link.com"
         }
     ];
