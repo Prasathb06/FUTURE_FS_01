72fa24ce4f3e7f0f1c451f4d31a3c734b4103da7
Prasathb06 <arunprasathb045@gmail.com>
2025-11-06 11:50:34 +0530

    first commit

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,8 +6,8 @@ const Footer = () => {
     return (
         <footer className="footer">
             <div className="social-icons">
-                <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
-                <a href="https://github.com" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
+                <a href="https://www.linkedin.com/in/arun-prasath-b-846b9a254/" target="_blank" rel="noopener noreferrer"><FaLinkedin /></a>
+                <a href="https://github.com/Prasathb06" target="_blank" rel="noopener noreferrer"><FaGithub /></a>
                 <a href="https://twitter.com" target="_blank" rel="noopener noreferrer"><FaTwitter /></a>
                 <a href="https://instagram.com" target="_blank" rel="noopener noreferrer"><FaInstagram /></a>
             </div>
